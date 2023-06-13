7576adb8cae54878e439140569597bf681d8ec02
joseph williams <wj882244@gmail.com>
2023-06-13 18:18:58 +0100

    update

--- src/Pages/Hairsalon/Hairsalonshop/Salondetails.jsx
+++ src/Pages/Hairsalon/Hairsalonshop/Salondetails.jsx
@@ -1,7 +1,8 @@
 import { useLocation } from "react-router-dom";
 import SalonDimage from "./SalonDImages";
-// import Navbar from '../../../components/Navbar'
-// import Footer from '../../../components/Footer'
+import Navbar from '../../../components/Navbar'
+import Footer from '../../../components/Footer'
+import Footbar from '../../../components/Footbar'
 // import HeroBottom from '../../../components/HomeComponents/HeroBottom'
 export default function Salondetails() {
   const location = useLocation();
@@ -9,17 +10,20 @@ export default function Salondetails() {
 
   return (
     <>
+    <div className='bg-[#1b1d21] h-[4rem] md:px-[2rem] pt-[1rem]'>
+        <Navbar/>
+      </div>
       {/* main tag for salon details  */}
       <main className="pt-9 px-2 flex gap-[5%]">
         <aside className="w-[65%]">
           {/* image slides */}
           <SalonDimage images={state.images} ratings={state.ratings} reviews={state.reviews}/>
-        <article className="mt-20">
+        <article className="mt-10">
             <h2 className="font-bold text-3xl font-serif text-zinc-700">{state.name}</h2>
             <p className="font-serif text-xs text-zinc-600">{state.address}</p>
             <span className="flex mt-11 justify-between gap-2">
                 <h2 className="text-3xl font-bold ">Services</h2>
-                <input type="search" name=""  className="font-bg font-serif bg-zinc-300 rounded-md outline-none flex w-[70%]" placeholder="Search for service"/>
+                <input type="search" name=""  className="font-bg font-serif bg-zinc-300 rounded-md outline-none flex md:w-[40%] w-70%" placeholder="Search for service"/>
             </span>
         </article>
         </aside>
@@ -32,6 +36,17 @@ export default function Salondetails() {
             </div>
         </aside>
       </main>
+            {/* FOOTER*/}
+      <div>
+        <Footer/>
+      </div>
+    <div 
+        className='w-full z-[9999999999999] lg:hidden fixed bottom-0 right-0'
+      >
+        <Footbar/>
+    </div>
+
+
     </>
   );
 }
