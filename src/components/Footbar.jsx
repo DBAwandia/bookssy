a79792cc120930df1f8a1899ea558d85950418ad
ken <kmaina510@gmail.com>
2023-06-13 19:53:54 +0300

    added

--- src/components/Footbar.jsx
+++ src/components/Footbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AiOutlineHeart } from "react-icons/ai"
 import { BiCalendar, BiSearch } from "react-icons/bi"
 import { RxPerson} from "react-icons/rx"
+import { Link } from 'react-router-dom'
 
 function Footbar() {
   return (
@@ -11,38 +12,50 @@ function Footbar() {
       <div
         className='px-[0.6rem] md:px-[1rem] py-[0.5rem] flex w-full justify-between items-center'
       >
-        <div
-            className='flex items-center flex-col text-[#8c8b88] gap-1' 
+        <Link 
+            to="/"
         >
-            <AiOutlineHeart
-                className='text-[1.3rem]'
-            />
-            <p>Your booksy</p>
-        </div>
-        <div
-            className='flex items-center flex-col text-[#8c8b88] gap-1' 
+            <div
+                className='flex items-center flex-col text-[#8c8b88] gap-1' 
+            >
+                <AiOutlineHeart
+                    className='text-[1.3rem]'
+                />
+                <p>Your booksy</p>
+            </div>
+        </Link>
+
+        <Link 
+            to="/salonshops"
         >
-            <BiSearch
-                className='text-[1.3rem]'
-            />
-            <p>Explore</p>
-        </div>
-        <div
-            className='flex items-center flex-col text-[#8c8b88] gap-1' 
-        >
-            <BiCalendar
-                className='text-[1.3rem]'
-            />
-            <p>Appointments</p>
-        </div>
-        <div
-            className='flex items-center flex-col text-[#8c8b88] gap-1' 
-        >
-            <RxPerson
-                className='text-[1.3rem]'
-            />
-            <p>Profile</p>
-        </div>
+            <div
+                className='flex items-center flex-col text-[#8c8b88] gap-1' 
+            >
+                <BiSearch
+                    className='text-[1.3rem]'
+                />
+                <p>Explore</p>
+            </div>
+        </Link>
+        
+            <div
+                className='flex items-center flex-col text-[#8c8b88] gap-1' 
+            >
+                <BiCalendar
+                    className='text-[1.3rem]'
+                />
+                <p>Appointments</p>
+            </div>
+
+            <div
+                className='flex items-center flex-col text-[#8c8b88] gap-1' 
+            >
+                <RxPerson
+                    className='text-[1.3rem]'
+                />
+                <p>Profile</p>
+            </div>
+
       </div>
     </div>
   )
