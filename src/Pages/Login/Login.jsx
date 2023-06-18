17bfc624e4c256fa62991223c62070ece049af2b
joseph williams <wj882244@gmail.com>
2023-06-18 14:29:09 +0100

    update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,6 +6,7 @@ export default function Login(props) {
         props.close()
     }
     const [activeTab, setActiveTab] = useState("emailInput");
+    const [showPassword, setShowpassword] = useState(false);
 
     const openTab = (tabName) => {
     setActiveTab(tabName);
@@ -27,6 +28,12 @@ return (
           <div className=" lg:absolute top-[10%]  z-[99999999999999999] lg:opacity-[0.95] lg:w-[40%] lg:h-[auto] lg:rounded-2xl bg-white lg:py-[2rem]">
             {activeTab === "emailInput"  && (
             <h2 className="text-center font-semibold text-lg font-sans pt-3 pb-[60px] select-none">My Email Address is:</h2> )}
+            {activeTab === "Done"  && (
+                <div>
+                    <h2 className="text-center font-semibold text-lg font-sans pt-3 pb-1 select-none">Please Level Contact details</h2>
+                    <p className="pb-[30px] text-center">In case you need to be contacted about your service</p>
+                </div>
+            )}
         <div className="flex justify-center gap-2">
             <span className="w-[40%] border-none bg-[#03929b] p-[2px] rounded-2xl"></span>
             <span className="w-[40%] border-none bg-[#03929b] p-[2px] rounded-2xl"></span>
@@ -40,8 +47,13 @@ return (
         </div>
         )}
         {activeTab === "Done" && (
-        <div>
-            <input className="block my-0 mx-auto border border-solid border-[#00000093] rounded-lg w-[80%] select-none" type="email" name="" id="" placeholder="Password"/>
+        <div className="pt-10">
+            <input className="block my-0 mx-auto border border-solid border-[#00000093] rounded-lg w-[80%]  select-none" type="email" name="" id="" placeholder="First and Last Name"/>
+
+            <br />
+            <input type="number" name="" id="" className="block my-0 mx-auto border border-solid border-[#00000093] rounded-lg w-[80%]  select-none" placeholder="Phone Number"/>
+            <br />
+            <input type="password" name="" id="" className="block my-0 mx-auto border border-solid border-[#00000093] rounded-lg w-[80%]  select-none" placeholder="Set Password"/>
 
             <button className="w-[90%] text-center bg-[#00a3ad] py-3 font-extrabold mx-auto my-0 block  rounded-xl text-white">Continue</button>
         </div>)}
